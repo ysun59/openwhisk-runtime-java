f1fd34a2d67b0f2e445ad29e3129440535b7bc45
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2019-10-16 09:04:01 +0200

    updating new benchmark, file hashing.

--- test-app/filehashing/FileHashing.java
+++ test-app/filehashing/FileHashing.java
@@ -3,23 +3,29 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Random;
-
+import java.io.InputStream;
 import com.google.gson.JsonObject;
+import io.minio.MinioClient;
 
 public class FileHashing {
 
 	private static final int size = 1024;
+	private static final byte[] buffer = new byte[size];
 
 	private static int run(int seed) {
 		try {
-			return Arrays.hashCode(Files.readAllBytes(Paths.get(String.format("/tmp/file-%d.dat", seed))));	
-		} catch(IOException e) {
+ 			MinioClient minioClient = new MinioClient("http://r630-02:9000", "keykey", "secretsecret");
+			InputStream stream = minioClient.getObject("mydata", String.format("file-%d.dat", seed));
+			int bytesread = 0;
+			while ((bytesread = stream.read(buffer, bytesread, size - bytesread)) >= 0) ;
+			return Arrays.hashCode(buffer);
+		} catch(Exception e) {
 			e.printStackTrace();
 			return 0;
 		}
-	}	
+	}
 
-public static JsonObject main(JsonObject args) {
+    public static JsonObject main(JsonObject args) {
     	int hash = 0;
 
     	if (args.has("seed")) {
@@ -31,3 +37,5 @@ public static JsonObject main(JsonObject args) {
     	return response;
     }
 }
+
+
--- test-app/filehashing/build.sh
+++ test-app/filehashing/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-
-$JAVA_HOME/bin/javac -cp ../../gson-2.8.5.jar FileHashing.java
+$JAVA_HOME/bin/javac -cp ../../gson-2.8.5.jar:../../minio-6.0.11-all.jar FileHashing.java
 $JAVA_HOME/bin/jar cvf filehashing.jar FileHashing.class
