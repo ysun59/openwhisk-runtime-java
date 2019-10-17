504ebbee762d93b094ef9c7fe8d28e2e64bb9a72
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2019-10-17 15:11:53 +0200

    minor scripts updates.

--- test-app/sort/init-openwhisk.sh
+++ test-app/sort/init-openwhisk.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-export WSK_CONFIG_FILE=/home/rbruno/git/incubator-openwhisk-devtools/docker-compose/.wskprops
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export OPENWHISK_HOME=/home/rbruno/git/openwhisk
+export WSK_CONFIG_FILE=/home/rbruno/git/incubator-openwhisk-devtools/docker-compose/.wskprops
+export OPENWHISK_HOME=/home/rbruno/git/incubator-openwhisk-devtools/docker-compose/openwhisk-src/
+wsk=$OPENWHISK_HOME/bin/wsk
 
-wsk action update -i Sort sort.jar --main Sort --docker rfbpb/java8action
+$wsk action create -i Sort sort.jar --main Sort --docker rfbpb/java8action -c 100
+#$wsk action update -i Sort sort.jar --main Sort --docker rfbpb/java8action -c 100
