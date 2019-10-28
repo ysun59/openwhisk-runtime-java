b3f51c427a42a96f8f893839e671fa07b969f2ec
Vojislav Dukic <voja91@gmail.com>
2019-10-28 17:10:24 +0100

    AWS lambda sleep added

---
+++ test-app/aws/sleep/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+(cd code && mvn package)
+
+aws lambda delete-function --function-name sleep-jvm
+
+aws lambda create-function --function-name sleep-jvm \
+--zip-file fileb://./code/target/sleep-1.0-SNAPSHOT.jar --handler Sleep::handleRequest --runtime java8 \
+--role arn:aws:iam::612948648635:role/service-role/hpy_lambda --memory-size 128
