8cb2576eb0d7c67dca929b48550704698b81b632
Vojislav Dukic <voja91@gmail.com>
2019-11-04 01:03:32 +0100

    FFMPEG fix

--- test-app/video/code/src/main/java/ch/ethz/FFMPEG.java
+++ test-app/video/code/src/main/java/ch/ethz/FFMPEG.java
@@ -4,8 +4,14 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.google.gson.JsonObject;
 
@@ -18,6 +24,32 @@ public class FFMPEG {
 
 	public static MinioClient minioClient = null;
 
+	private static String readAllBytes(String filePath){
+        String content = "";
+        try{
+            content = new String ( Files.readAllBytes( Paths.get(filePath) ) );
+        }
+        catch (IOException e) {}
+        return content;
+    }
+	
+	public static double current_utilization_runtime(){
+        String meminfo = readAllBytes("/proc/meminfo").replace("\n", "");
+        //total
+        Pattern p = Pattern.compile("MemTotal: *(.*?) kB");
+        Matcher m = p.matcher(meminfo);
+        m.find();
+        long memory_total = Long.parseLong(m.group(1));
+        
+        //free
+        p = Pattern.compile("MemAvailable: *(.*?) kB");
+        m = p.matcher(meminfo);
+        m.find();
+        long memory_free = Long.parseLong(m.group(1));
+        return ((memory_total - memory_free)/1000.);
+        //return (long)((memory_total)/1000.);
+    }
+	
 	private static void copyInputStreamToFile(InputStream inputStream, File file) throws IOException {
 
 		try (FileOutputStream outputStream = new FileOutputStream(file)) {
@@ -38,14 +70,20 @@ public class FFMPEG {
 			minioClient = new MinioClient("http://r630-02:9000", "keykey", "secretsecret");
 			InputStream is = minioClient.getObject("video", "ffmpeg");
 			copyInputStreamToFile(is, new File("ffmpeg"));
+			Process process = Runtime.getRuntime().exec("chmod +x ./ffmpeg");
+			process.waitFor();
 		} catch (Exception e) {
-			e.printStackTrace();
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			e.printStackTrace(pw);
+			String sStackTrace = sw.toString(); // stack trace as a string
+			throw new RuntimeException("1" + sStackTrace);
 		}
 
 	}
 
 	private static void ffmpeg(String fileName) throws Exception{
-		FFmpeg ffmpeg = new FFmpeg("ffmpeg");
+		FFmpeg ffmpeg = new FFmpeg("./ffmpeg");
 
 		FFmpegBuilder builder = new FFmpegBuilder()
 
@@ -74,7 +112,11 @@ public class FFMPEG {
 			ffmpeg(fileName);
 
 		} catch (Exception e) {
-			e.printStackTrace();
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			e.printStackTrace(pw);
+			String sStackTrace = sw.toString(); // stack trace as a string
+			throw new RuntimeException("2" + sStackTrace);
 		}
 
 	}
@@ -86,6 +128,7 @@ public class FFMPEG {
 
 	public static JsonObject main(JsonObject args, Map<String, Object> globals, int id) {
 		boolean slow_start = true;
+		double m0 = current_utilization_runtime();
 		synchronized (globals) {
 			if (!globals.containsKey("minio")) {
 				init_classifier();
@@ -95,10 +138,19 @@ public class FFMPEG {
 				slow_start = false;
 			}
 		}
+		double m1 = current_utilization_runtime();
 
 		transform(id);
+		
+		double m2 = current_utilization_runtime();
 		JsonObject response = new JsonObject();
+		long runtime_mem= Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
 		response.addProperty("slow_start", slow_start ? 1 : 0);
+		response.addProperty("runtime_m", Double.toString(runtime_mem/1000000.));
+		response.addProperty("m0", Double.toString(m1));
+		response.addProperty("m1", Double.toString(m1));
+		response.addProperty("m2", Double.toString(m2));
+		response.addProperty("exists", new File("ffmpeg").exists());
 		return response;
 	}
 }
