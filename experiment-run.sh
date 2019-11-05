718dac8ab6a048a0da94610f9ae778f9192e61fe
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2019-11-05 09:49:53 +0100

    pushing scripts and changes for final results.

--- experiment-run.sh
+++ experiment-run.sh
@@ -20,33 +20,67 @@ function run {
     # step 3: run loadrunner
     cd tools; ./loadrunner-jvm.py -nt $cthread -ne $nexps -nw $nwarms | tee $wrk_dir/$tag.log | grep -v "#"; cd - &> /dev/null
 
+    docker cp $app_name:/tmp/container.gc $wrk_dir/$tag.gc
+    mkdir $wrk_dir/$tag.logs
+    docker cp $app_name:/tmp $wrk_dir/$tag.logs
+
     # step 4: kill docker
     docker kill $app_name
     docker rm $app_name
 }
 
 mkdir $wrk_dir &> /dev/null
-rm $wrk_dir/*
+rm -r $wrk_dir/*
 docker pull rfbpb/java8action
+
+function filehashing {
+    if   [ "$cthread" = "1"  ]; then vcpu="0.1"; mem="192m"
+        elif [ "$cthread" = "2"  ]; then vcpu="0.2"; mem="384m"
+        elif [ "$cthread" = "4"  ]; then vcpu="0.4"; mem="768m"
+        elif [ "$cthread" = "6"  ]; then vcpu="0.6"; mem="1088m"
+        elif [ "$cthread" = "8"  ]; then vcpu="0.8"; mem="1472m"
+        elif [ "$cthread" = "10" ]; then vcpu="1";   mem="1792m"
+        elif [ "$cthread" = "12" ]; then vcpu="1.2"; mem="2176m"
+        elif [ "$cthread" = "14" ]; then vcpu="1.4"; mem="2560m"
+        elif [ "$cthread" = "16" ]; then vcpu="1.6"; mem="2880m"
+        else echo "Unknown vcpu config..."; exit 1
+    fi
+}
+
+function imageclassification {
+    if   [ "$cthread" = "1"  ]; then vcpu="0.2";
+        elif [ "$cthread" = "2"  ]; then vcpu="0.4";
+        elif [ "$cthread" = "4"  ]; then vcpu="0.8";
+        elif [ "$cthread" = "6"  ]; then vcpu="1.2";
+        elif [ "$cthread" = "8"  ]; then vcpu="1.6";
+        else echo "Unknown vcpu config..."; exit 1
+    fi
+    mem="1024m"
+}
+
+function video {
+    if   [ "$cthread" = "1"  ]; then vcpu="0.1";
+        elif [ "$cthread" = "2"  ]; then vcpu="0.2";
+        elif [ "$cthread" = "4"  ]; then vcpu="0.4";
+        elif [ "$cthread" = "6"  ]; then vcpu="0.6";
+        elif [ "$cthread" = "8"  ]; then vcpu="0.8";
+        elif [ "$cthread" = "10"  ]; then vcpu="1.0";
+        elif [ "$cthread" = "12"  ]; then vcpu="1.2";
+        elif [ "$cthread" = "14"  ]; then vcpu="1.4";
+        elif [ "$cthread" = "16"  ]; then vcpu="1.6";
+        else echo "Unknown vcpu config..."; exit 1
+    fi
+    mem="512m"
+}
 
 for cthread in $cthreads
 do
-    if   [ "$cthread" = "1"  ]; then vcpu="0.1"; mem="192m"
-    elif [ "$cthread" = "2"  ]; then vcpu="0.2"; mem="384m"
-    elif [ "$cthread" = "4"  ]; then vcpu="0.4"; mem="768m"
-    elif [ "$cthread" = "6"  ]; then vcpu="0.6"; mem="1088m"
-    elif [ "$cthread" = "8"  ]; then vcpu="0.8"; mem="1472m"
-    elif [ "$cthread" = "10" ]; then vcpu="1";   mem="1792m"
-    elif [ "$cthread" = "12" ]; then vcpu="1.2"; mem="2176m"
-    elif [ "$cthread" = "14" ]; then vcpu="1.4"; mem="2560m"
-    elif [ "$cthread" = "16" ]; then vcpu="1.6"; mem="2880m"
-    else echo "Unknown vcpu config..."; exit 1
-    fi
+    filehashing
     for rep in $reps
     do
         tag="vcpu-$vcpu-conc-$cthread-rep-$rep"
         echo "running $wrk_dir/$tag.log ..."
-        #run
+        run
         echo "running $wrk_dir/$tag.log ... done"
     done
 done
--- test-app/video/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-#export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-$JAVA_HOME/bin/javac -cp ../../gson-2.8.5.jar Sleep.java
-$JAVA_HOME/bin/jar cvf sleep.jar Sleep.class
