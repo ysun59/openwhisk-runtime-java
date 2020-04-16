c7c1f12eb660c61eb696ee7412d4755b4883e033
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2020-04-16 12:35:32 +0200

    improving file hashing benchmark.

--- test-app/filehashing/FileHashing.java
+++ test-app/filehashing/FileHashing.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.Random;
 import java.io.InputStream;
+import java.security.MessageDigest;
+import javax.xml.bind.DatatypeConverter;
 import com.google.gson.JsonObject;
 import io.minio.MinioClient;
 
@@ -13,30 +15,52 @@ public class FileHashing {
 	private static final int size = 1024;
 	private static final byte[] buffer = new byte[size];
 
-	private static int run(int seed) {
+    private static MinioClient init() {
+        try {
+            return new MinioClient("http://10.1.212.71:9000", "keykey", "secretsecret");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // TODO - is this Minioclient thread safe?
+	private static String run(MinioClient minioClient, int seed) {
 		try {
- 			MinioClient minioClient = new MinioClient("http://r630-02:9000", "keykey", "secretsecret");
-			InputStream stream = minioClient.getObject("mydata", String.format("file-%d.dat", seed));
-			int bytesread = 0;
-			while ((bytesread = stream.read(buffer, bytesread, size - bytesread)) >= 0) ;
-			return Arrays.hashCode(buffer);
+			InputStream stream = minioClient.getObject("files", String.format("file-%d.dat", seed));
+            for (int bytesread = 0;
+                 bytesread < size;
+                 bytesread += stream.read(buffer, bytesread, size - bytesread));
+            stream.close();
+            return DatatypeConverter.printHexBinary(MessageDigest.getInstance("MD5").digest(buffer));
 		} catch(Exception e) {
 			e.printStackTrace();
-			return 0;
 		}
+        return null;
 	}
 
     public static JsonObject main(JsonObject args, Map<String, Object> globals, int id) {
-    	int hash = 0;
+    	String hash = null;
+        MinioClient minioClient;
+        long time = System.currentTimeMillis();
 
-        System.out.println(String.format("time %d id %d", globals.get("time"), id));
+        //System.out.println(String.format("time %d id %d", globals.get("time"), id));
 
-    	if (args.has("seed")) {
-    		hash = run(args.getAsJsonPrimitive("seed").getAsInt());
+        synchronized (globals) {
+            if (!globals.containsKey("minio")) {
+                minioClient = init();
+                globals.put("minio", minioClient);
+            } else {
+                minioClient = (MinioClient) globals.get("minio");
+            }
+        }
+
+        if (args.has("seed")) {
+    		hash = run(minioClient, args.getAsJsonPrimitive("seed").getAsInt());
     	}
 
     	JsonObject response = new JsonObject();
-    	response.addProperty("hash", String.format("%d", hash));
+    	response.addProperty("hash", hash);
+    	response.addProperty("time", System.currentTimeMillis() - time);
     	return response;
     }
 }
