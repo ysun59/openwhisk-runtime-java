d2df7a17a0b55088824b025e56093906d6e20f0b
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2020-05-01 13:50:17 +0200

    updating minio populate to include files for more tests.

--- minio-populate.sh
+++ minio-populate.sh
@@ -3,10 +3,15 @@
 datadir="/tmp/minio-data/files"
 mkdir -p $datadir
 
+# for the filehashing test
 for i in {1..100}
 do
 	openssl rand 1024 > $datadir/file-$i.dat
 done
 
+# for the video test
+cp res/ffmpeg $datadir
+cp res/*.mp4 $datadir
+
 #~/local/mc mb minio/mydata
-#~/local/mc cp ~/local/minio-data/* minio/mydata 
+#~/local/mc cp ~/local/minio-data/* minio/mydata
